0fe3cc89f9be87401006bd78fd38e86fa35dd6e1
mvxxx <michal.tyrolski@gmail.com>
2020-05-17 22:23:49 +0200

    ml api

--- model/api/mlhat.py
+++ model/api/mlhat.py
@@ -1,12 +1,13 @@
 from keras.utils.data_utils import get_file
 from tensorflow.keras.models import load_model
 from abc import ABC, abstractmethod
+import json
 
 class MLHat(ABC):
     def __init__(self, url, arch_id):
         self._model = load_model(get_file(arch_id, url))
 
-    def __len__(self):
+    def len(self):
         return self._model.count_params()
 
     @property
@@ -18,17 +19,19 @@ class MLHat(ABC):
         return len(self._feature_paths)
 
     @abstractmethod
-    def predict(input):
+    def predict(self, input):
         pass
         
 
 class HatClassifier(MLHat):
-    def init(self, url, arch_id, **model_kwargs):
-        super().__init__(url, name) 
+
+    def __init__(self, url, arch_id, **model_kwargs):
+        super().__init__(url, arch_id) 
         self._model_kwargs = model_kwargs
+
     
     
-    def predict(input):
+    def predict(self, input):
         """
         Predicts if input represents hat or not
 
@@ -41,5 +44,7 @@ class HatClassifier(MLHat):
         Raises:
             Internal Keras Error if input is invalid
         """
-        [hat, nothat] = self._model.predict(input)
-        return hat > nothat
+        print(self._model.predict(input))
+        [[hat, nothat]] = self._model.predict(input)
+        model_out = {"pred":('hat' if hat > nothat else 'nothat')}
+        return json.dumps(model_out)
