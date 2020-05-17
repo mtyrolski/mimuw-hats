ca1d33e5a85e265da8db52a49a789254436f9a1d
Michal Makowski <makosek@poczta.onet.pl>
2020-05-17 18:56:31 +0200

    somewhat working reactions

--- client/my-app/src/Reaction.tsx
+++ client/my-app/src/Reaction.tsx
@@ -11,12 +11,36 @@ export interface ReactionInstance {
     user: User;
 }
 
-let reactions: Reaction[];
-export async function getReactions() {
-    if (reactions) return reactions;
+// let reactions: Map<number, Reaction>;
+// export async function getReactions(): Promise<Map<number, Reaction>> {
+//     if (reactions) return reactions;
+//
+//     reactions = new Map();
+//     // TODO handle error
+//     await fetch('http://localhost:2137/reactions')
+//         .then(response => response.json())
+//         .then(json => json.map((reaction: Reaction) => {
+//             reactions.set(reaction.id, reaction);
+//         }))
+//
+//     return reactions;
+// }
+//
 
-    reactions = await fetch('http://localhost:2137/reactions')
-        .then(response => response.json());
-    return reactions;
-}
-
+export const reactions: Map<number, Reaction> = new Map([
+    [0, {
+        imageID: 'hamt.png',
+        name: 'cheems',
+        id: 0
+    }],
+    [1, {
+        imageID: 'tso.png',
+        name: 'tso',
+        id: 1
+    }],
+    [2, {
+        imageID: 'papiez.png',
+        name: 'papiez',
+        id: 2
+    }]
+]);
--- client/my-app/src/ReactionBox.tsx
+++ client/my-app/src/ReactionBox.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {Post} from './Post';
-import {Reaction} from "./Reaction";
+import {Reaction, reactions} from "./Reaction";
 import {Badge} from "antd";
+import assert from "assert";
 
 interface Props {
     post: Post;
@@ -15,47 +16,68 @@ export interface PostReactionWrapper {
 }
 
 export function getPostReactions(post: Post): Array<PostReactionWrapper> {
-    return [];
-    // let map: Map<Reaction, PostReactionWrapper> = new Map();
+    let map: Map<number, PostReactionWrapper> = new Map();
 
-    // if (!post.reactions) return [];
+    reactions.forEach((value: Reaction, key: number) => {
+        map.set(key, {
+            count: 0,
+            fromCurrentUser: false,
+            post: post,
+            reaction: value
+        })
+    })
 
-    // for (let reactionInstance of post.reactions) {
-    //     let instance = map.get(reactionInstance.reaction);
-    //     if (!instance) {
-    //         instance = {
-    //             count: 0,
-    //             fromCurrentUser: false,
-    //             post: post,
-    //             reaction: reactionInstance.reaction
-    //         };
-    //         map.set(reactionInstance.reaction, instance);
-    //     }
 
-    //     instance.count++;
-    //     // TODO
-    //     // if (reactionInstance.user == currentUser) {
-    //     //     instance.fromCurrentUser = true;
-    //     // }
-    // }
+    for (let reactionInstance of post.reactions) {
+        let instance = map.get(reactionInstance.id)!;
 
-    // return Array.from(map.values());
+        instance.count++;
+        // TODO
+        // if (reactionInstance.user == currentUser) {
+        //     instance.fromCurrentUser = true;
+        // }
+    }
+
+    return Array.from(map.values());
 }
 
 export class ReactionBox extends React.Component<Props> {
+    state = {
+        reactions: []
+    }
+
+    componentDidMount() {
+        this.setState({reactions: getPostReactions(this.props.post)});
+    }
+
+    toggleReaction(reactionWrapper: PostReactionWrapper) {
+        if (reactionWrapper.fromCurrentUser) {
+            reactionWrapper.count--;
+            reactionWrapper.fromCurrentUser = false;
+        } else {
+            reactionWrapper.count++;
+            reactionWrapper.fromCurrentUser = true;
+        }
+        this.forceUpdate();
+        // TODO make request, also forceUpdate bad
+    }
+
     render() {
         return (
-            <div>reactions</div>
-            // <div>
-            //     {
-            //         getPostReactions(this.props.post).map(
-            //             (reactionWrapper: PostReactionWrapper) =>
-            //                 <Badge count={reactionWrapper.count}>
-            //                     <img style={{height: '2em'}} alt={reactionWrapper.reaction.name} src={"/images/" + reactionWrapper.reaction.imageID} />
-            //                 </Badge>
-            //         )
-            //     }
-            // </div>
+            <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                {
+                    this.state.reactions.map(
+                        (reactionWrapper: PostReactionWrapper) => {
+                            return <a onClick={() => this.toggleReaction(reactionWrapper)}>
+                                    <Badge count={reactionWrapper.count}>
+                                    <img style={{height: '2em'}} alt={reactionWrapper.reaction.name}
+                                         src={'/images/' + reactionWrapper.reaction.imageID}/>
+                                </Badge>
+                            </a>;
+                        }
+                    )
+                }
+            </div>
         )
     }
 }
